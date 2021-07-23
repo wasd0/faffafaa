8f0def6038feff0a1f7291a2f28174fc2aaf7108
Shvedov Kirill <84603952+wasd0@users.noreply.github.com>
2021-07-24 02:36:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faffafaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Points(levels));
+            Console.ReadKey();
+        }
+        static int Points(int levels)
+        {
+            int exp = 0;
+            for (int i = 0; i < levels; i++)
+            {
+                exp = i + exp + 1;
+            }
+            return exp;
+        }
+
+    }
+}
